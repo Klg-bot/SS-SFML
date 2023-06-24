7ddbb93f7bf7c6b290fc459ac08b8fdd8269d471
Yaohan <yaohan0824@gmail.com>
2023-06-25 00:03:20 +0800

    speed changes

--- Game 3/Enemy.cpp
+++ Game 3/Enemy.cpp
@@ -5,7 +5,7 @@ void Enemy::initVariables()
 {
     this->pointCount = rand() % 8 + 3; //min = 3 max = 10
 	this->type		= 0;
-	this->speed		= static_cast<float>(10); //this->pointCount/3
+	this->speed		= static_cast<float>(this->pointCount/3 + 5); 
 	this->hpMax		= static_cast<int>(this->pointCount);
 	this->hp		= this->hpMax;
 	this->damage	= this->pointCount;
